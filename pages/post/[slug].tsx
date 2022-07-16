6dc3d5a5debe8555f8d1446fa26bdded69247a70
Cagri Oz <cagriozarpaciii@hotmail.com>
2022-07-16 20:31:14 +0300

    single post elements

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -2,15 +2,82 @@ import Header from '../../components/Header';
 import { sanityClient } from '../../lib/sanity.server';
 import { Post } from '../../typings';
 import { GetStaticProps } from 'next';
+import { urlFor } from '../../lib/sanity';
+import { PortableText as PortableTextComponent } from '@portabletext/react';
+import { getImageDimensions } from '@sanity/asset-utils';
+import urlBuilder from '@sanity/image-url';
+
+// Barebones lazy-loaded image component
+const ImageComponent = ({ value }: any) => {
+    const { width, height } = getImageDimensions(value);
+    return (
+        <img
+            src={urlBuilder(sanityClient).image(value).width(width).height(height).url()}
+            alt={value.alt || ' '}
+            loading="lazy"
+        />
+    );
+};
+
+const PortableText = (props: any) => (
+    <PortableTextComponent
+        components={{
+            block: {
+                h1: ({ children }: any) => <h1 className="text-2xl font-bold my-5">{children}</h1>,
+                h2: ({ children }: any) => <h2 className="text-xl font-bold my-5">{children}</h2>,
+                li: ({ children }: any) => <li className="ml-4 list-disc">{children}</li>,
+                link: ({ href, children }: any) => (
+                    <a href={href} className="text-blue-500 hover:underline">
+                        {children}
+                    </a>
+                ),
+                p: ({ children }: any) => <p className="text-base my-5">{children}</p>,
+            },
+            types: {
+                image: ImageComponent,
+            },
+        }}
+        {...props}
+    />
+);
 
 interface Props {
     post: Post;
 }
 
 function Post({ post }: Props) {
+    console.log(post.body);
     return (
         <main>
             <Header />
+
+            <img className="w-full h-80 object-cover" src={urlFor(post.mainImage).url()!} alt={post.title} />
+
+            <article className="max-w-3xl mx-auto p-5">
+                <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
+                <h2 className="text-xl font-light text-gray-500 mb-2">{post.description}</h2>
+
+                <div className="flex items-center space-x-2">
+                    <img
+                        className="h-10 w-10 rounded-full object-cover"
+                        src={urlFor(post.author.image).url()}
+                        alt={post.author.name}
+                    />
+                    <p className="font-extralight text-sm">
+                        Blog post by <span className="text-green-600">{post.author.name}</span> - Published at{' '}
+                        {new Date(post._createdAt).toLocaleString()}
+                    </p>
+                </div>
+
+                <div>
+                    <PortableText
+                        className=""
+                        dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+                        projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+                        value={post.body}
+                    />
+                </div>
+            </article>
         </main>
     );
 }
@@ -47,8 +114,12 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
         slug {
             current
         },
+        'comments': *[
+            _type == 'comment' && post._ref == ^._id && approved == true
+        ],
         mainImage,
         description,
+        body,
         author -> {
             name,
             image
